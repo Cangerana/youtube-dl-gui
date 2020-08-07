f8e0b7ac7178b24c09798e0811409647410e2aca
Cangerana <mrjoaovictorc@gmail.com>
2020-08-07 20:46:10 -0300

    Seted the basic of viwer.py

--- view.py
+++ view.py
@@ -1,12 +1,48 @@
 from tkinter import *
 
 
-# root Window
-root = Tk()
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.set_geometry()
+        self.set_titles('Youtube Downloader')
+        # self.set_menu()
+        self.set_download_button()
 
-# Criating label widget
-my_label = Label(root, text='Hello World!')
-# Shoving it onto the screen
-my_label.pack()
+    def set_titles(self, title):
+        self.master.title(title)
 
-root.mainloop()
+    def set_geometry(self):
+        self.master.geometry('630x320')
+
+    def set_download_button(self):
+        def click():
+            click_label = Label(self.master, text='Download button was ckliked!')
+            click_label.pack()
+        
+
+        button = Button(self.master, text='Download', fg='red', command=click)
+        button.place(x=525, y=280)
+        # button.pack()
+
+    def set_menu(self):
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        fileMenu = Menu(menu)
+        fileMenu.add_command(label="Item")
+        fileMenu.add_command(label="Exit")
+        menu.add_cascade(label="File", menu=fileMenu)
+
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Undo")
+        editMenu.add_command(label="Redo")
+        menu.add_cascade(label="Edit", menu=editMenu)
+
+
+if __name__ == '__main__':
+    # App loop
+    root = Tk()
+    app = Window(root)
+    root.mainloop()
