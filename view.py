e3bfa69b7222e7d8b9a7fac94ffd7f2f1795aa9a
Cangerana <mrjoaovictorc@gmail.com>
2020-08-07 16:23:58 -0300

    Created a window with 'Hello World'code

---
+++ view.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+# root Window
+root = Tk()
+
+# Criating label widget
+my_label = Label(root, text='Hello World!')
+# Shoving it onto the screen
+my_label.pack()
+
+root.mainloop()
