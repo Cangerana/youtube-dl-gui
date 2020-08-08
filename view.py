ae78e0ecd60901e18d98aa128cf31404ecb7c48a
Cangerana <mrjoaovictorc@gmail.com>
2020-08-07 23:07:27 -0300

    Added box to entry with the video url

--- view.py
+++ view.py
@@ -9,22 +9,26 @@ class Window(Frame):
         self.set_titles('Youtube Downloader')
         # self.set_menu()
         self.set_download_button()
+        self.set_url_entry()
 
     def set_titles(self, title):
         self.master.title(title)
 
     def set_geometry(self):
-        self.master.geometry('630x320')
+        self.master.geometry('640x300')
+
+    def set_url_entry(self):
+        self.url_entry = Entry(self.master, width=50, borderwidth=3)
+        self.url_entry.pack(padx=40, ipady=5)
 
     def set_download_button(self):
         def click():
-            click_label = Label(self.master, text='Download button was ckliked!')
+            click_label = Label(self.master, text=self.url_entry.get())
             click_label.pack()
-        
 
-        button = Button(self.master, text='Download', fg='red', command=click)
-        button.place(x=525, y=280)
-        # button.pack()
+        button = Button(self.master, text='Download',
+                        fg='white', bg='red', command=click)
+        button.place(x=525, y=250)
 
     def set_menu(self):
         menu = Menu(self.master)
