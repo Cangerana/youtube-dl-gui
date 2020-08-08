8397ee3c6c37ef02cfbad6b63126a2fa04ef89fe
Cangerana <mrjoaovictorc@gmail.com>
2020-08-08 16:28:55 -0300

    Seted button to select a destination folder and event handler

--- view.py
+++ view.py
@@ -1,23 +1,29 @@
 from tkinter import *
-import yt_dl_conections
-from yt_dl_conections import Reader
+from tkinter import filedialog
+
+from yt_dl_conections import EventHandler
+
 
 class Window(Frame):
-    def __init__(self, master=None, reader=None):
+    def __init__(self, master=None, handle=None):
         Frame.__init__(self, master)
-        self.reader = reader
         self.master = master
+        self.handle = handle
+
+        self.download_path = '~/Downloads'
+
         self.set_geometry()
         self.set_titles('Youtube Downloader')
-        # self.set_menu()
-        self.set_download_button()
         self.set_url_entry()
-
-    def set_titles(self, title):
-        self.master.title(title)
+        self.set_download_button()
+        self.set_folder_selector()
+        self.set_format_selector()
 
     def set_geometry(self):
-        self.master.geometry('640x300')
+        self.master.geometry('640x200')
+    
+    def set_titles(self, title):
+        self.master.title(title)
 
     def set_url_entry(self):
         self.url_entry = Entry(self.master, width=50, borderwidth=3)
@@ -25,31 +31,34 @@ class Window(Frame):
         self.url_entry.pack(padx=40, ipady=5)
 
     def set_download_button(self):
-        def click():
-            reader.download_button_reader(self.url_entry.get())
+        def donwload():
+            self.handle.download_button_handler(self.url_entry.get())
 
-        button = Button(self.master, text='Download',
-                        fg='white', bg='red', command=click)
-        button.place(x=525, y=250)
+        button = Button(self.master, text='Download', fg='white', bg='red', command=donwload)
+        button.place(x=525, y=150)
 
-    def set_menu(self):
-        menu = Menu(self.master)
-        self.master.config(menu=menu)
+    def set_folder_selector(self):
+        def select():
+            folder_selector = filedialog.Directory(initialdir=self.download_path, title='Select a folder to download')
+            folder_selector.show()
+            path = folder_selector.__getattribute__('directory')
+            
+            if path != '':
+                folder['text'] = path
 
-        fileMenu = Menu(menu)
-        fileMenu.add_command(label="Item")
-        fileMenu.add_command(label="Exit")
-        menu.add_cascade(label="File", menu=fileMenu)
+                self.handle.folder_selector_handler(path)
 
-        editMenu = Menu(menu)
-        editMenu.add_command(label="Undo")
-        editMenu.add_command(label="Redo")
-        menu.add_cascade(label="Edit", menu=editMenu)
+
+        folder = Button(self.master, text=self.download_path, width=30 ,command=select)
+        folder.place(x=40, y=150)
+
+    def set_format_selector(self):
+        pass
 
 
 if __name__ == '__main__':
     # App loop
     root = Tk()
-    reader = Reader()
-    app = Window(root, reader)
+    handler = EventHandler()
+    app = Window(root, handler)
     root.mainloop()
--- yt_dl_conections.py
+++ yt_dl_conections.py
@@ -1,11 +1,15 @@
 from tkinter import Event
 
 
-class Reader():
+class EventHandler():
     def __init__(self):
         self.url = ''
         self.dir = '~/Download'
 
-    def download_button_reader(self, event):
+    def download_button_handler(self, event):
         self.url = event
         print(self.url)
+
+    def folder_selector_handler(self, event):
+        self.path = event
+        print(self.path)
