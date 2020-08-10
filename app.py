1e961252a58e964bdd696efbd94d9e2304ab2641
Cangerana <mrjoaovictorc@gmail.com>
2020-08-10 15:07:29 -0300

    Created the main class in the app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from tkinter import Tk
+
+from view import Window
+
+class App():
+    """Main class of the program"""
+
+    def __init__(self) -> None:
+        self.master = Tk()
+        self.window = Window(self.master)
+
+    def run(self):
+        """This function start the program"""
+
+        self.master.mainloop()
+
+
+if __name__ == '__main__':
+    main = App()
+    main.run()
